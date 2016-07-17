aa3ff0be10c392aa4a092f9bbcd453bae360cc08
kelsonic <kgadams93@gmail.com>
2016-07-17 14:37:23 -0700

    Redirect user to index page once post is deleted

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,20 +1,50 @@
-import React, {Component} from 'react';
+import React, {Component, PropTypes} from 'react';
 import {connect} from 'react-redux';
-import {fetchPost} from '../actions/index';
+import {fetchPost, deletePost} from '../actions/index';
+import {Link} from 'react-router';
 
 class PostsShow extends Component {
   
+  static contextTypes = {
+    router: PropTypes.object
+  };
+
   componentWillMount() {
     this.props.fetchPost(this.props.params.id);
   }
 
+  onDeleteClick() {
+    this.props.deletePost(this.props.params.id)
+      .then(() => {
+        this.context.router.push('/');
+      });
+  }
+
   render() {
+    const {post} = this.props;
+
+    if (!post) {
+      return <div>Loading...</div>;
+    }
+
     return (
       <div>
-        Show post {this.props.params.id}
+        <Link to="/">Back to Posts</Link>
+        <h3>{post.title}</h3>
+        <h6>Categories: {post.categories}</h6>
+        <p>{post.content}</p>
+        <button 
+          onClick={this.onDeleteClick.bind(this)} 
+          className="btn btn-danger pull-xs-right">
+          Delete Post
+        </button>
       </div>
     );
   }
 }
 
-export default connect(null, {fetchPost})(PostsShow);
+function mapStateToProps(state) {
+  return {post: state.posts.post};
+}
+
+export default connect(mapStateToProps, {fetchPost, deletePost})(PostsShow);
